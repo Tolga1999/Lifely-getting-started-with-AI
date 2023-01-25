c12046c326ffabcfeff1df1925707a51514b14ac
Tolga1999 <tolgadoganer@outlook.com>
2023-01-25 03:24:42 +0100

    steps JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
-console.log("het werkt")
+// buttons
+let nextButton = document.querySelector("#next-button");
+let previousButton = document.querySelector("#previous-button");
+
+// step containers
+let step1Container = document.querySelector(".step-1-container");
+let step2Container = document.querySelector(".step-2-container");
+
+// next button
+nextButton.addEventListener('click', function () {
+    step2Container.classList.add("display-next-container");
+    step1Container.classList.add("remove-previous-container");
+    // check previous button
+    previousButton.classList.remove("disable-button");
+    previousButton.disabled = false;
+
+})
+
+// previous button
+previousButton.addEventListener('click', function () {
+    step2Container.classList.remove("display-next-container");
+    step1Container.classList.remove("remove-previous-container");
+
+    // check previous button
+    previousButton.classList.add("disable-button");
+    previousButton.disabled = true;
+})
